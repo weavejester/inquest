098936fb02e2708c77e80e9ea6dff16f1d3bd6ca
James Reeves <jreeves@weavejester.com>
2014-11-01 23:50:14 +0000

    Add basic monitor function

--- src/inquest/core.clj
+++ src/inquest/core.clj
@@ -1,6 +1,9 @@
 (ns inquest.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn- wrap-monitor [f var]
+  (fn [& args]
+    (prn {:var var, :args args})
+    (apply f args)))
+
+(defn monitor [var]
+  (alter-var-root var wrap-monitor var))
