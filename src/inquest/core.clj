eafc25fa70a894af839f528fe0bae1d08c2e939d
James Reeves <jreeves@weavejester.com>
2014-11-02 14:28:19 +0000

    Improve efficiency of adding many monitors to a function

--- src/inquest/core.clj
+++ src/inquest/core.clj
@@ -1,14 +1,23 @@
 (ns inquest.core)
 
-(defn- wrap-monitor [f var callback]
+(defn- report [callbacks message]
+  (doseq [c callbacks] (c message)))
+
+(defn- wrap-monitor [f var callbacks]
   (with-meta
     (fn [& args]
-      (callback {:var var, :args args})
+      (report @callbacks {:var var, :args args})
       (apply f args))
-    {::original f}))
+    {::original f
+     ::callbacks callbacks}))
+
+(defn- add-callback [f var callback]
+  (if-let [cbs (-> f meta ::callbacks)]
+    (do (swap! cbs conj callback) f)
+    (wrap-monitor f var (atom #{callback}))))
 
 (defn monitor [var callback]
-  (alter-var-root var wrap-monitor var callback))
+  (alter-var-root var add-callback var callback))
 
 (defn unmonitor [var]
   (alter-var-root var (comp ::original meta)))
