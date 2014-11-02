c7dc47024a15f5cb92f4c9e5f47347ae67edeabd
James Reeves <jreeves@weavejester.com>
2014-11-02 02:33:18 +0000

    Add unmonitor function to reverse monitor

--- src/inquest/core.clj
+++ src/inquest/core.clj
@@ -1,9 +1,14 @@
 (ns inquest.core)
 
 (defn- wrap-monitor [f var callback]
-  (fn [& args]
-    (callback {:var var, :args args})
-    (apply f args)))
+  (with-meta
+    (fn [& args]
+      (callback {:var var, :args args})
+      (apply f args))
+    {::original f}))
 
 (defn monitor [var callback]
   (alter-var-root var wrap-monitor var callback))
+
+(defn unmonitor [var]
+  (alter-var-root var (comp ::original meta)))
