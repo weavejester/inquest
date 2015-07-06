2c65a51fa65add059dace1c9bf16069fb393dceb
James Reeves <jreeves@weavejester.com>
2015-07-06 18:32:37 +0100

    Change minimum Clojure version to 1.5.1

--- project.clj
+++ project.clj
@@ -3,4 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :aliases {"test-all" ["do"
+                        ["test"]
+                        ["with-profile" "+1.6" "test"]
+                        ["with-profile" "+1.7" "test"]]}
+  :profiles
+  {:1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+   :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}})
