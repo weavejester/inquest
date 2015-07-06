4c5e292a5d962883d55915c0f9345e726d036d31
James Reeves <jreeves@weavejester.com>
2015-07-06 18:26:48 +0100

    Add test for inquest

--- test/inquest/core_test.clj
+++ test/inquest/core_test.clj
@@ -3,6 +3,7 @@
             [inquest.core :refer :all]))
 
 (defn foo [x] (+ x 1))
+(defn bar [x] (- x 1))
 
 (deftest test-monitor
   (let [reports (atom [])]
@@ -36,3 +37,34 @@
     (unmonitor #'foo ::test)
     (is (= (foo 1) 2))
     (is (= (count @reports) 0))))
+
+(deftest test-inquest
+  (let [reports (atom [])
+        stop    (inquest [#'foo #'bar] #(swap! reports conj %))]
+    (testing "started"
+      (try
+        (is (= (foo 1) 2))
+        (is (= (bar 2) 1))
+        (is (= (count @reports) 4))
+        (let [r (@reports 0)]
+          (is (= (:var r) #'foo))
+          (is (= (:state r) :enter))
+          (is (= (:args r) '(1))))
+        (let [r (@reports 1)]
+          (is (= (:var r) #'foo))
+          (is (= (:state r) :exit))
+          (is (= (:return r) 2)))
+        (let [r (@reports 2)]
+          (is (= (:var r) #'bar))
+          (is (= (:state r) :enter))
+          (is (= (:args r) '(2))))
+        (let [r (@reports 3)]
+          (is (= (:var r) #'bar))
+          (is (= (:state r) :exit))
+          (is (= (:return r) 1)))
+        (finally
+          (stop))))
+    (testing "stopped"
+      (is (= (foo 1) 2))
+      (is (= (bar 2) 1))
+      (is (= (count @reports) 4)))))
