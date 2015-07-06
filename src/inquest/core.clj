4bbb0c13b274038a9abb8fc1d9b18c7839fe4002
James Reeves <jreeves@weavejester.com>
2015-07-06 14:40:30 +0100

    Add docstrings

--- src/inquest/core.clj
+++ src/inquest/core.clj
@@ -1,4 +1,5 @@
-(ns inquest.core)
+(ns inquest.core
+  "Functions to add monitoring to existing vars.")
 
 (defn- report [state var key value]
   {:time   (System/nanoTime)
@@ -28,19 +29,45 @@
 (defn- monitor-reporters [var]
   (-> var var-get meta ::reporters))
 
-(defn monitor [var key reporter]
+(defn monitor
+  "Monitors an existing var, passing reports to a reporter function. A report
+  is a map consisting of at least the following keys:
+
+    :time   - the system time in nanoseconds
+    :thread - the thread ID
+    :state  - one of :enter, :exit or :throw
+    :var    - the var being monitored
+
+  Additionally there may be the following optional keys:
+
+    :args      - the arguments passed to the var (only in the :enter state)
+    :return    - the return value from the var   (only in the :exit state)
+    :exception - the thrown exception            (only in the :throw state)
+
+  The key argument may be anything, and is used with the unmonitor function
+  to remove the reporter from the var."
+  [var key reporter]
   (locking var
     (if-let [reporters (monitor-reporters var)]
       (swap! reporters assoc key reporter)
       (alter-var-root var wrap-monitor var (atom {key reporter})))))
 
-(defn unmonitor [var key]
+(defn unmonitor
+  "Removes the reporter identified by the key from the var, setting it back to
+  normal."
+  [var key]
   (locking var
     (when-let [reporters (monitor-reporters var)]
       (when (empty? (swap! reporters dissoc key))
         (alter-var-root var (comp ::original meta))))))
 
-(defn inquest [vars reporter]
+(defn inquest
+  "A convenience function for monitoring many vars with the same reporter
+  function. The return value is a zero-argument function that, when called,
+  will remove all of the monitoring added for the inquest.
+
+  See the monitor function for an explanation of the reporter function."
+  [vars reporter]
   (let [key (gensym "inquest-")]
     (doseq [v vars]
       (monitor v key reporter))
