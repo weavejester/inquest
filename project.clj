60dd2f6a2b60428d90c4bc6ef9b5c487dd757e92
James Reeves <jreeves@weavejester.com>
2015-07-08 16:59:58 +0100

    Add Criterium to dev dependencies

--- project.clj
+++ project.clj
@@ -12,4 +12,5 @@
                         ["with-profile" "+1.7" "test"]]}
   :profiles
   {:1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-   :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}})
+   :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+   :dev {:dependencies [[criterium "0.4.3"]]}})
